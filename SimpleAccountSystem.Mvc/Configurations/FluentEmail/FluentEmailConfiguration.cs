60cc7a75c5893b231c1d7f542e09550354242bc4
Nyrico Angelo M. Cuartel <nyrico@sdsolutions.com.ph>
2023-06-05 22:55:29 +0800

    added fluent email configuration

---
+++ SimpleAccountSystem.Mvc/Configurations/FluentEmail/FluentEmailConfiguration.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.Extensions.Configuration;
+using SimpleAccountSystem.Mvc.Configurations.Models;
+using System.Net;
+
+namespace SimpleAccountSystem.Mvc.Configurations.FluentEmail
+{
+    public static class FluentEmailConfiguration
+    {
+        public static void BuildFluentEmailDependencies(this IServiceCollection services,ConfigurationManager configManager)
+        {
+            FluentEmailConfigurationModel configModel = configManager.GetSection("FluentEmailHostConfig").Get<FluentEmailConfigurationModel>();
+
+            services.AddFluentEmail(configModel.EmailUserName).AddSmtpSender(new System.Net.Mail.SmtpClient(configModel.SmtpHost)
+                                                                {
+                                                                    UseDefaultCredentials = configModel.SmtpUseDefaultCredentials,
+                                                                    Port = configModel.SmtpPort,
+                                                                    EnableSsl = configModel.EnableSsl,
+                                                                    Credentials = new NetworkCredential(configModel.EmailUserName,configModel.EmailPassword)
+                                                                });
+        }
+    }
+}
