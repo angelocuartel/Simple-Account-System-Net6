93182566cece996d8b9df1a1b0814af5da27d6cf
Nyrico Angelo M. Cuartel <angelocuartel@gmail.com>
2023-07-16 00:55:23 +0800

    added step up auth for accessing admin modules

---
+++ SimpleAccountSystem.Mvc/Attributes/StepUpAuthenticationAttribute.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.AspNetCore.Mvc.Filters;
+using System.Security.Claims;
+
+namespace SimpleAccountSystem.Mvc.Attributes
+{
+    [AttributeUsage(AttributeTargets.Class | AttributeTargets.Method)]
+    public class StepUpAuthenticationAttribute : Attribute, IAuthorizationFilter
+    {
+        public const string stepUpPath = "stepUpPath";
+        public void OnAuthorization(AuthorizationFilterContext context)
+        {
+            var user = context.HttpContext.User;
+
+            if (user != null)
+            {
+                var name = user.FindFirst(ClaimTypes.Name)?.Value;
+                var requestPath = context.HttpContext.Session.GetString(name + stepUpPath);
+
+                if (!string.IsNullOrEmpty(requestPath) && context.HttpContext.Request.Path.Equals(requestPath))
+                {
+                    context.HttpContext.Session.Remove(name + stepUpPath);
+                    return;
+
+                }
+
+                context.HttpContext.Response.Redirect("/Identity/Account/LoginWith2fa?returnUrl=" + context.HttpContext.Request.Path);
+                return;
+            }
+
+            context.HttpContext.Response.Redirect("/Identity/Account/Login");
+        }
+    }
+}
