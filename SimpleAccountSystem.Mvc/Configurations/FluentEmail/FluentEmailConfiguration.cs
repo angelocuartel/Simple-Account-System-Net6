bc37d8f8d00ae13df8064007a44feab0bf8b144d
Nyrico Angelo M. Cuartel <angelocuartel@gmail.com>
2023-07-15 21:02:04 +0800

    removed unused imports

--- SimpleAccountSystem.Mvc/Configurations/FluentEmail/FluentEmailConfiguration.cs
+++ SimpleAccountSystem.Mvc/Configurations/FluentEmail/FluentEmailConfiguration.cs
@@ -1,5 +1,4 @@
-﻿using Microsoft.Extensions.Configuration;
-using SimpleAccountSystem.Mvc.Configurations.Constants;
+﻿using SimpleAccountSystem.Mvc.Configurations.Constants;
 using SimpleAccountSystem.Mvc.Configurations.Models;
 using System.Net;
 
@@ -7,17 +6,17 @@ namespace SimpleAccountSystem.Mvc.Configurations.FluentEmail
 {
     public static class FluentEmailConfiguration
     {
-        public static void BuildFluentEmailDependencies(this IServiceCollection services,ConfigurationManager configManager)
+        public static void BuildFluentEmailDependencies(this IServiceCollection services, ConfigurationManager configManager)
         {
             FluentEmailConfigurationModel configModel = configManager.GetSection(ConfigurationConstant.FluentEmail).Get<FluentEmailConfigurationModel>();
 
             services.AddFluentEmail(configModel.EmailUserName).AddSmtpSender(new System.Net.Mail.SmtpClient(configModel.SmtpHost)
-                                                                {
-                                                                    UseDefaultCredentials = configModel.SmtpUseDefaultCredentials,
-                                                                    Port = configModel.SmtpPort,
-                                                                    EnableSsl = configModel.EnableSsl,
-                                                                    Credentials = new NetworkCredential(configModel.EmailUserName,configModel.EmailPassword)
-                                                                });
+            {
+                UseDefaultCredentials = configModel.SmtpUseDefaultCredentials,
+                Port = configModel.SmtpPort,
+                EnableSsl = configModel.EnableSsl,
+                Credentials = new NetworkCredential(configModel.EmailUserName, configModel.EmailPassword)
+            });
         }
     }
 }
