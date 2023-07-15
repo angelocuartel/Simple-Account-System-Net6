b42f1219c83c5e369ed7c41beb0ca66564bb2aef
Nyrico Angelo M. Cuartel <angelocuartel@gmail.com>
2023-07-16 00:54:57 +0800

    added user module

---
+++ SimpleAccountSystem.Mvc/Controllers/UserController.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using SimpleAccountSystem.Mvc.Attributes;
+
+namespace SimpleAccountSystem.Mvc.Controllers
+{
+    [Authorize]
+    [StepUpAuthentication]
+    public class UserController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
