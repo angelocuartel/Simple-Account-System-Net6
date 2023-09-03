dc725536b2ea9a564f8ed0538d4430b38f47ba84
Nyrico Angelo M. Cuartel <angelocuartel@gmail.com>
2023-09-03 22:39:13 +0800

    display user list

---
+++ SimpleAccountSystem.Mvc/Commons/GenericListResultDto.cs
@@ -0,0 +1,9 @@
+﻿namespace SimpleAccountSystem.Mvc.Commons
+{
+    public class GenericListResultDto<T>
+    {
+        public IEnumerable<T>? List { get; set; }
+        public int Page { get; set; } = 10;
+        public int Offset { get; set; } = 1;
+    }
+}
--- SimpleAccountSystem.Mvc/Controllers/UserController.cs
+++ SimpleAccountSystem.Mvc/Controllers/UserController.cs
@@ -1,6 +1,8 @@
 ﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
 using SimpleAccountSystem.Mvc.Attributes;
+using SimpleAccountSystem.Mvc.Commons;
 
 namespace SimpleAccountSystem.Mvc.Controllers
 {
@@ -8,9 +10,27 @@ namespace SimpleAccountSystem.Mvc.Controllers
     [StepUpAuthentication]
     public class UserController : Controller
     {
+        private readonly UserManager<IdentityUser> _userManager;
+
+        public UserController(UserManager<IdentityUser> userManager)
+        {
+            _userManager = userManager;
+        }
+
         public IActionResult Index()
         {
-            return View();
+            var users = _userManager.Users.ToList();
+
+            var genericResult = new GenericListResultDto<IdentityUser>();
+            genericResult.List = users;
+
+            return View(genericResult);
         }
+
+        //[HttpGet]
+        //public IActionResult FilterByRoles()
+        //{
+
+        //}
     }
 }
