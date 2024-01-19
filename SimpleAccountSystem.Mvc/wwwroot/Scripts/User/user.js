4e9a3e2c1f7bd466543b38b02ab093eeab1a56a3
Nyrico Angelo M. Cuartel <angelocuartel@gmail.com>
2024-01-19 23:12:12 +0800

    implement modal add user

---
+++ SimpleAccountSystem.Mvc/wwwroot/Scripts/User/user.js
@@ -0,0 +1,45 @@
+﻿$(document).ready(() => {
+
+    const table = new DataTable('#user-table', {
+
+        //pagination settings
+        pagingType: 'full_numbers',
+
+        // server side processing settings
+        ajax: {
+            url: `User/GetUsers`,
+            type: 'GET',
+            contentType: 'application/x-www-form-urlencoded',
+            dataType: 'json'
+        },
+        processing: true,
+        serverSide: true,
+
+        // columns settings
+        columns: [
+            { data: "userName" },
+            { data: "email" },
+            {
+                data: "twoFactorEnabled",
+                render: (data, type) => {
+                    if (data)
+                        return "enabled";
+                    else
+                        return "disabled";
+                }
+            },
+            {
+                data: "actions",
+                render: (data, type) => {
+                    return "<btn class = 'btn btn-danger'>Remove</btn>";
+                }
+            },
+        ]
+
+    });
+
+    $('#btn-add-user').on('click', () => {
+        $('#user-modal').modal('show');
+    });
+
+});
