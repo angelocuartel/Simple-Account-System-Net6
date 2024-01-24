748da82553b96d62f21de19ba9b65f20bfae5498
Nyrico Angelo M. Cuartel <angelocuartel@gmail.com>
2024-01-24 21:42:35 +0800

    added custom method that will handle model validation using FluentValidation

---
+++ SimpleAccountSystem.Mvc/Controllers/Base/CustomControllerBase.cs
@@ -0,0 +1,36 @@
+﻿using FluentValidation;
+using FluentValidation.Results;
+using Microsoft.AspNetCore.Mvc;
+
+namespace SimpleAccountSystem.Mvc.Controllers.Base
+{
+    public class CustomControllerBase : Controller
+    {
+        protected async Task<ValidationResult> ValidateModelAsync<Tmodel, Tvalidator>(Tmodel model) where Tvalidator : IValidator<Tmodel>
+        {
+            if(model is null)
+            {
+                new ValidationResult() 
+                {
+                    Errors = new List<ValidationFailure>
+                    {
+                        new ValidationFailure(string.Empty,"model is required") 
+                    } 
+                };
+            }
+            
+            var validator = Activator.CreateInstance<Tvalidator>();
+
+            return await validator.ValidateAsync(model);
+        }
+
+        protected IActionResult FluentBadRequest(List<ValidationFailure> errors)
+        {
+            var firstError = errors.FirstOrDefault()?.ErrorMessage;
+            return BadRequest(firstError);
+        }
+        
+
+        
+    }
+}
